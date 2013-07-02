1b1cfa6f938f9a0e108b1c5ed5aec58c48c860cd
Austin Rochford <austin.rochford@gmail.com>
2013-07-02 02:09:05 +0000

    Refactored to use open

--- MontyHall.hs
+++ MontyHall.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 import Prelude hiding (map)
 import Data.List ((\\))
 import Numeric.Probability.Distribution (just, map, T, uniform, (>>=?))
@@ -7,12 +9,14 @@ data Door = First | Second | Third deriving (Show, Eq, Ord)
 prior :: (Fractional p) => T p Door
 prior = uniform [First, Second, Third]
 
+open :: (Fractional p) => Door -> Door -> T p Door
+open chosen correct = uniform $ otherDoors [chosen, correct]
+
 joint :: (Fractional p) => Door -> Door -> T p (Door, Door)
-joint chosen correct = uniform $ zip (repeat correct) (otherDoors [chosen, correct])
+joint chosen correct = map (correct,) $ open chosen correct
 
 posterior :: (Fractional p) => Door -> Door -> T p Door
 posterior chosen opened = map fst $ prior >>= joint chosen >>=? just opened . snd
 
---utilities
 otherDoors :: [Door] -> [Door]
 otherDoors = (\\) [First, Second, Third]
