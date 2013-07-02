b480ecfc6f0e47d1ee206dddde7b615d4f856897
Austin Rochford <austin.rochford@gmail.com>
2013-07-02 02:30:50 +0000

    Added posteriorMax and change

--- MontyHall.hs
+++ MontyHall.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE TupleSections #-}
 
 import Prelude hiding (map)
-import Data.List ((\\))
-import Numeric.Probability.Distribution (just, map, T, uniform, (>>=?))
+import Data.List (maximumBy, (\\))
+import Data.Ord (comparing)
+import Numeric.Probability.Distribution (decons, just, map, T, uniform, (>>=?))
 
 data Door = First | Second | Third deriving (Show, Eq, Ord)
 
@@ -18,5 +19,16 @@ joint chosen correct = map (correct,) $ open chosen correct
 posterior :: (Fractional p) => Door -> Door -> T p Door
 posterior chosen opened = map fst $ prior >>= joint chosen >>=? just opened . snd
 
+posteriorMax :: Door -> Door -> Door
+posteriorMax = fst . maximumBy (comparing snd) . decons ## posterior
+
+change :: Door -> Door -> Bool
+change chosen = (chosen ==) . posteriorMax chosen
+
+--utilities
 otherDoors :: [Door] -> [Door]
 otherDoors = (\\) [First, Second, Third]
+
+infixr 8 ##
+(##) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(##) = (.) . (.)
