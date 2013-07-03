0c1687b510f4f0adb0ef931db850e164cc44ebe1
Austin Rochford <austin.rochford@gmail.com>
2013-07-03 02:30:31 +0000

    Changed >>+ to joint and added bayes

--- MontyHall.hs
+++ MontyHall.hs
@@ -5,7 +5,7 @@ import Control.Applicative ((<$>))
 import Data.List (maximumBy, (\\))
 import Data.Ord (comparing)
 
-import Numeric.Probability.Distribution (decons, just, T, uniform, (>>=?))
+import Numeric.Probability.Distribution (decons, Event, just, T, uniform, (>>=?))
 
 data Door = First | Second | Third deriving (Show, Eq, Ord)
 
@@ -15,8 +15,8 @@ prior = uniform [First, Second, Third]
 open :: (Fractional p) => Door -> Door -> T p Door
 open chosen correct = uniform $ otherDoors [chosen, correct]
 
-posterior :: (Fractional p) => Door -> Door -> T p Door
-posterior chosen opened = fst <$> (prior >>+ open chosen >>=? just opened . snd)
+--posterior :: (Fractional p) => Door -> Door -> T p Door
+--posterior chosen opened = fst <$> (prior >>+ open chosen >>=? just opened . snd)
 
 --door utilities
 otherDoors :: [Door] -> [Door]
@@ -26,8 +26,11 @@ otherDoors = (\\) [First, Second, Third]
 graph :: (Num p, Ord b, Ord a) => (a -> T p b) -> (a -> T p (a, b))
 graph f x = (x,) <$> f x
 
-(>>+) :: (Num p, Ord b, Ord a) => T p a -> (a -> T p b) -> T p (a, b)
-dist >>+ f = dist >>= graph f
+joint :: (Num p, Ord b, Ord a) => T p a -> (a -> T p b) -> T p (a, b)
+joint dist cond = dist >>= graph cond
+
+bayes :: (Fractional p, Ord b, Ord a) => Event b -> T p a -> (a -> T p b) -> T p a
+bayes event dist cond = fst <$> (joint dist cond >>=? event . snd)
 
 --miscellaneous utilities
 infixr 8 ##
